175a8a4b64ae7ab4e74868aef7731d4bee4b5706
Brijesh99094 <88873013+Brijesh99094@users.noreply.github.com>
2022-08-14 09:12:09 +0530

    Update settings.py

--- Bww_v1/settings.py
+++ Bww_v1/settings.py
@@ -131,9 +131,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join('static'),
+    os.path.join(BASE_DIR,'static'),
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
